34db0a6fa96281ee25d867086784cb54daec5cdc
zueore <zueore@gmail.com>
2017-05-18 14:32:20 +0530

    Hash

--- server/database/mongoose.js
+++ server/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-var dbname = 'mongodb://tododb:dbtodo87@ds035059.mlab.com:35059/todoapp' || 'mongodb://localhost:27017/todoAPI';
+var dbname = /*'mongodb://tododb:dbtodo87@ds035059.mlab.com:35059/todoapp' || */'mongodb://localhost:27017/todoAPI';
 
 mongoose.connect(dbname, (e) => {
   if (e) throw e;
