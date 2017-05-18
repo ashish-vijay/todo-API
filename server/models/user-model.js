161c0b4c7869c856f3b94a01dd1a8dcc6f15a05e
zueore <zueore@gmail.com>
2017-05-18 22:17:05 +0530

    Improved for production

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -31,7 +31,11 @@ var UserSchema = new mongoose.Schema({
       type: String,
       required: true
     }
-  }]
+  }],
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  }
 });
 
 UserSchema.methods.toJSON = function() {
@@ -44,7 +48,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SALT).toString();
 
   user.tokens.push({access, token});
 
@@ -68,7 +72,7 @@ UserSchema.statics.findByToken = function(token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SALT);
   } catch(e) {
     return Promise.reject();
   }
--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new todoschema({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((docs) => {
@@ -28,20 +29,25 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  todoschema.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  todoschema.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({todos});
   }, (e) => {
       res.status(400).send(err);
     });
 });
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   if(!ObjectID.isValid(id)) {
     return res.status(404).send('Invalid id');
   }
-  todoschema.findById(id).then((todos) => {
+  todoschema.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todos) => {
       if(!todos) {
         return res.status(404).send();
       }
@@ -51,12 +57,15 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate,(req, res) => {
   var id = req.params.id;
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  todoschema.findOneAndRemove(id).then((todos) => {
+  todoschema.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todos) => {
     if(!todos) {
       return res.status(404).send();
     }
@@ -66,7 +75,7 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -82,7 +91,10 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  todoschema.findOneAndUpdate(id, {$set: body}, {new: true}).then((todos) => {
+  todoschema.findOneAndUpdate({
+    _id: id,
+    _creator: req.user._id
+  }, {$set: body}, {new: true}).then((todos) => {
     if(!todos) {
       return res.status(404).send();
     }
